388d283a5a2abbf1fbe0eeed4a71982fe2a393c2
PraktikantRalle <rasmus.hellgren@ingka.com>
2024-12-04 11:37:40 +0100

    begin day 4

---
+++ 4/first.py
@@ -0,0 +1,7 @@
+with open("4/input.txt", "r") as input_file:
+  input = input_file.read()
+
+print(input)
+
+# I'm thinking making input into a matrix, then maybe looping through each index, checking the letters/indices around index to see if they're the next letter in XMAS
+# Idk this one seems hard
